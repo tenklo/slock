b3cd5c02344b241dd5d0f2460cdbe8c9fb5d542c
tenklo <tenklo@pm.me>
2020-11-28 09:29:15 +0100

    added xresources & quickcancel patch

--- config.h
+++ config.h
@@ -9,5 +9,18 @@ static const char *colorname[NUMCOLS] = {
 	[CAPS] = "yellow",         /* CapsLock on */
 };
 
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "color3",       STRING,  &colorname[CAPS] },
+};
+
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* time in seconds to cancel lock with mouse movement */
+static const int timetocancel = 4;
